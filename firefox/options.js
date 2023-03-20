15f5e1b2ac2942c02ac76d40e5ecadf06154b4fa
Josde <contact@josde.me>
2023-03-20 14:36:17 +0100

    test

--- firefox/options.js
+++ firefox/options.js
@@ -1,5 +1,5 @@
 import { DefaultOptions } from '../shared.js';
-
+// TODO: Change back browser.storage.local to browser.storage.sync before release
 // restore state from storage
 document.addEventListener("DOMContentLoaded", () => {
 	browser.storage.sync.get(DefaultOptions).then(items => {
@@ -20,24 +20,4 @@ document.getElementById("block-following").addEventListener("input", () => {
 	});
 });
 
-document.getElementById("skip-verified").addEventListener("input", () => {
-	browser.storage.sync.set({
-		skipVerified: document.getElementById("skip-verified").checked,
-	}).then(() => {
-		// Update status to let user know options were saved.
-		const status = document.getElementById("skip-verified-status");
-		status.textContent = "saved";
-		setTimeout(() => status.textContent = null, 1000);
-	});
-});
 
-document.getElementById("block-nft-avatars").addEventListener("input", () => {
-	browser.storage.sync.set({
-		blockNftAvatars: document.getElementById("block-nft-avatars").checked,
-	}).then(() => {
-		// Update status to let user know options were saved.
-		const status = document.getElementById("block-nft-avatars-status");
-		status.textContent = "saved";
-		setTimeout(() => status.textContent = null, 1000);
-	});
-});
